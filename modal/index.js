edebc0058df820bbbbcf18fbcb51512b86c9038c
Jayesh <jayeshrathi6055@gmail.com>
2021-11-20 13:39:15 +0530

    [added] Modal

---
+++ modal/index.js
@@ -0,0 +1,10 @@
+
+function launchModal(ModalId) {
+    let modalId = document.getElementById(ModalId);
+    modalId.style.display = "grid"
+}
+
+function closeModal(ModalId) {
+    let modalId = document.getElementById(ModalId);
+    modalId.style.display = "none"
+}
